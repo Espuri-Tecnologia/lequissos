bb9aa10cc7b913999453c4ed22801b9068d91a01
viniespuri <58437762+viniespuri@users.noreply.github.com>
2025-07-03 10:56:44 -0300

    Add EventType initialization and tests

---
+++ tests/LexosHub.ERP.VarejoOnline.Domain.Tests/Messaging/EventTypeTests.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using LexosHub.ERP.VarejoOnline.Infra.Messaging.Dispatcher;
+using LexosHub.ERP.VarejoOnline.Infra.Messaging.Events;
+using Xunit;
+
+namespace LexosHub.ERP.VarejoOnline.Domain.Tests.Messaging
+{
+    public class EventTypeTests
+    {
+        public static IEnumerable<object[]> Events => new List<object[]>
+        {
+            new object[] { new IntegrationCreated() },
+            new object[] { new CompaniesRequested() },
+            new object[] { new ProductsRequested() },
+            new object[] { new ProductsPageProcessed() },
+            new object[] { new InitialSync() }
+        };
+
+        [Theory]
+        [MemberData(nameof(Events))]
+        public void Constructor_Should_Set_EventType_To_Class_Name(BaseEvent evt)
+        {
+            Assert.Equal(evt.GetType().Name, evt.EventType);
+        }
+
+        [Theory]
+        [MemberData(nameof(Events))]
+        public void EventTypeResolver_Should_Deserialize_To_Correct_Type(BaseEvent evt)
+        {
+            var json = JsonSerializer.Serialize(evt);
+            var envelope = JsonSerializer.Deserialize<BaseEvent>(json);
+            var actualType = EventTypeResolver.Resolve(envelope!.EventType);
+            var deserialized = (BaseEvent)JsonSerializer.Deserialize(json, actualType)!;
+
+            Assert.IsType(evt.GetType(), deserialized);
+            Assert.Equal(evt.EventType, deserialized.EventType);
+        }
+    }
+}
