19d6f39d3b0dfa7c5f4ac54351d491272c54e3f9
viniespuri <58437762+viniespuri@users.noreply.github.com>
2025-07-07 11:31:23 -0300

    Add VarejoOnlineProduto mapper and tests

---
+++ src/LexosHub.ERP.VarejoOnline.Domain/Models/Produto/VarejoOnlineProdutoDto.cs
@@ -0,0 +1,14 @@
+namespace Lexos.Hub.Sync.Models.Produto
+{
+    public class VarejoOnlineProdutoDto
+    {
+        public long id { get; set; }
+        public string? descricao { get; set; }
+        public string? descricaoSimplificada { get; set; }
+        public string? codigoBarras { get; set; }
+        public decimal? peso { get; set; }
+        public decimal? comprimento { get; set; }
+        public decimal? largura { get; set; }
+        public decimal? altura { get; set; }
+    }
+}
